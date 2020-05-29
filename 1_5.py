eec80179c01222176e7325e7ee050ce5b2a11890
kvora125 <41047220+kvora125@users.noreply.github.com>
2020-05-29 19:57:57 +0530

    Create 1_5.py

---
+++ 1_5.py
@@ -0,0 +1,11 @@
+lines = [
+    "Burning 'em, if you ain't quick and nimble\n",
+    "I go crazy when I hear a cymbal",
+]
+
+text = "".join(lines)
+key = bytearray("ICE" * len(text))
+plaintext = bytes(xor(bytearray(text), key))
+plaintext.encode("hex")
+# 0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f
+6. Break repeating-key XOR
